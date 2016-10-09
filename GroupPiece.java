b904c5f5a8b30b5f39471013e7894bcb0016d274
Melissa van Staalduinen <vanstaalduinenm@Melissas-MacBook-Air.local>
2016-10-09 10:50:58 -0600

    Initial setup for Group class

---
+++ GroupPiece.java
@@ -0,0 +1,41 @@
+package abpuzzle;
+
+public class GroupPiece {
+
+	private int sum;
+	private int totalTokens;
+	private int smallDisk;
+	private boolean complete;
+
+	GroupPiece (Piece token)
+	// pass the circular array. from the circular array can count the
+	// number of like tokens.
+	{
+		smallDisk = token.getSmallDisk();
+		complete = false;
+		// totalTokens = count from circle array
+	}
+
+	public int getSum()
+	{
+		return sum;
+	}
+
+	public void setComplete()
+	{
+		complete = true;
+	}
+
+	public void removeComplete()
+	{
+		complete = false;
+	}
+
+	public boolean isComplete()
+	{
+		return complete;
+	}
+
+
+
+}
