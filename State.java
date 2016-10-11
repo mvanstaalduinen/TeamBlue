1f83c65ab824de69475906969f93bd86273174ab
Melissa van Staalduinen <vanstaalduinenm@Melissas-MacBook-Air.local>
2016-10-11 12:14:43 -0600

    made setFriends final so can use in constructor

--- State.java
+++ State.java
@@ -48,7 +48,7 @@ public class State {
         }
     }
     
-    public int setFriends()
+    public final int setFriends()
     {
         int friends = 0;
         for (int i = 0; i < state.size(); i++) {
